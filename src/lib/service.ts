17321954df01e825391b3d4de54236117ec6028a
rus_anonym <alexandrsemin2033@gmail.com>
2021-10-16 05:42:19 +0300

    make countryCode public in Service

--- src/lib/service.ts
+++ src/lib/service.ts
@@ -7,22 +7,22 @@ import {
 import { IServiceOptions, TServiceProcess } from "../types/bomber";
 
 class Service {
-	private _countryCode: CountryCode;
+	public countryCode: CountryCode;
 	private _process: TServiceProcess;
 
 	constructor(params: IServiceOptions) {
-		this._countryCode = params.country;
+		this.countryCode = params.country;
 		this._process = params.process;
 	}
 
 	public checkPhone(phone: string) {
-		return isValidPhoneNumber(phone, this._countryCode);
+		return isValidPhoneNumber(phone, this.countryCode);
 	}
 
 	public async init(phone: string) {
-		const phoneNumber = parsePhoneNumber(phone, this._countryCode);
+		const phoneNumber = parsePhoneNumber(phone, this.countryCode);
 
-		if (phoneNumber.country !== this._countryCode) {
+		if (phoneNumber.country !== this.countryCode) {
 			throw new Error("Phone is not valid");
 		} else {
 			await this._process(phoneNumber);
