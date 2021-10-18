3f2ecf08fffccb21fd053bf0db940b39b34faf8e
rus_anonym <alexandrsemin2033@gmail.com>
2021-10-19 01:54:04 +0300

    add carsmile

---
+++ src/services/RU/carsmile.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+import Service from "../../lib/service";
+
+export default new Service({
+	country: "RU",
+	isSupportProxyAgent: true,
+	async process(phone, additional) {
+		return await axios({
+			httpsAgent: additional.proxyAgent,
+			method: "POST",
+			url: `https://api.carsmile.com`,
+			headers: {
+				"Content-Type": "application/json",
+				//User agent causes an error
+			},
+			data: {
+				operationName: "enterPhone",
+				variables: {
+					phone: "7" + phone.nationalNumber,
+				},
+				query:
+					"mutation enterPhone($phone: String!) {\n  enterPhone(phone: $phone)\n}\n",
+			},
+		});
+	},
+});
