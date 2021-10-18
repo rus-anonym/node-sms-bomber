4357f26aa148188fdf03624e78e6ee20faa884f0
rus_anonym <alexandrsemin2033@gmail.com>
2021-10-19 01:45:45 +0300

    add anytimeprime

---
+++ src/services/RU/anytimeprime.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+import Service from "../../lib/service";
+
+export default new Service({
+	country: "RU",
+	isSupportProxyAgent: true,
+	async process(phone, additional) {
+		return await axios({
+			httpsAgent: additional.proxyAgent,
+			method: "POST",
+			url: "https://api-prime.anytime.global/api/v2/auth/sendVerificationCode",
+			headers: {
+				"Content-Type": "application/x-www-form-urlencoded",
+				//User agent causes an error
+			},
+			data: `phone=7${phone.nationalNumber}`,
+		});
+	},
+});
